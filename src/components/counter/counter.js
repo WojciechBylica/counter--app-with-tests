923d111b583eb2e3c915a4efb4ab262bd897a05b
Wojciech Bylica <wojtekbyl@gmail.com>
2022-03-19 02:36:50 +0100

    renamed counter/setCounter. added data-test

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import counter from './counter.css';
 
 const Counter = ({ start }) => {
-  const [counter, setCounter] = useState(start);
+  const [result, setResult] = useState(start);
   const [newInitialNumber, setNewInitialNumber] = useState('');
 
-  const handleAddition = () => setCounter(counter + 1);
+  const handleAddition = () => setResult(result + 1);
 
-  const handleSubtraction = () => setCounter(counter - 1);
+  const handleSubtraction = () => setResult(result - 1);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    setCounter(newInitialNumber);
+    setResult(newInitialNumber);
   };
 
   const handleReset = () => {
-    setCounter(start);
+    setResult(start);
     setNewInitialNumber('');
   };
 
@@ -30,7 +30,9 @@ const Counter = ({ start }) => {
         >
           +
         </button>
-        <p className="counter__paragraph">{counter}</p>
+        <p data-test="result" className="counter__paragraph">
+          {result}
+        </p>
         <button
           className="counter__button counter__button--big"
           data-test="subtractionButton"
@@ -40,7 +42,7 @@ const Counter = ({ start }) => {
         </button>
       </div>
 
-      <form className="counter__form" onSubmit={onFormSubmit}>
+      <form data-test="form" className="counter__form" onSubmit={onFormSubmit}>
         <input
           className="counter__input"
           data-test="input"
